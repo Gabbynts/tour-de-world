b0fa3c37f4fbebdfb71be8079cf0632c9b98e2b4
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-18 01:48:40 +0700

    feat: world photos url

---
+++ src/constant/photoWorlds.ts
@@ -0,0 +1,27 @@
+export const photoWorlds = [
+    {
+        id: '1',
+        imgUrl: '/images/paris.png',
+        title: 'Paris'
+    },
+    {
+        id: '2',
+        imgUrl: '/images/desert.png',
+        title: 'Desert'
+    },
+    {
+        id: '3',
+        imgUrl: '/images/waterfall.png',
+        title: 'Waterfall'
+    },
+    {
+        id: '4',
+        imgUrl: '/images/newyork.png',
+        title: 'NewYork'
+    },
+    {
+        id: '5',
+        imgUrl: '/images/everest.png',
+        title: 'Everest'
+    }
+];
