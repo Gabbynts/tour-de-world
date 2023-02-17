ac067c61ff134538953813e4da03439f3aa66035
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-17 22:04:57 +0700

    feat: layout wrap components

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type LayoutProps = {
+    children: React.ReactNode,
+    withFooter?: boolean,
+    withHeader?: boolean
+}
+
+function Layout({children, withFooter = true, withHeader = true}:LayoutProps) {
+  return (
+    <>
+    {withHeader && <Header />}
+        {children}
+    {withFooter && <Footer />}
+    </>
+  )
+}
+
+export default Layout
