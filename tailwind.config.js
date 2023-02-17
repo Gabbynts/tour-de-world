58a409967c0644f7964cb75daaf1a18f658dba8b
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-17 23:31:06 +0700

    feat: layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
-    colors: {
-      primary: {
-        100: '#DFAE93',
-        200: '#be602c',
-        300: '#3b2e27'
-      },
-      secondary: '#D9D9D9',
-    }
+    // colors: {
+    //   primary: {
+    //     100: '#DFAE93',
+    //     200: '#be602c',
+    //     300: '#3b2e27'
+    //   },
+    //   secondary: {
+    //     100: '#D9D9D9',
+    //     200: ''},
+    // }
   },
   plugins: [],
 }
