f841bd0b9bb9532c689328ffb51ec8fab3684566
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-18 19:29:22 +0700

    feat: apply text component

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,11 +1,17 @@
+import Text from '@/components/Text';
 import Image from 'next/image';
 import React from 'react';
 
 function About() {
   return (
-    <section id='' className='bg-gradient-to-b from-[#D9D9D9] to-[#c59a82] rounded-tl-[700px] z-10'>
-      <div className='hidden sm:block gradient-02 z-0 relative overflow-hidden right-0 -bottom-[500px]' />
-      <div className='relative pt-20 flex justify-end'>
+    <section
+      id=''
+      className=' flex bg-gradient-to-b from-[#D9D9D9] to-[#c59a82] rounded-tl-[700px] z-10 items-center w-full'
+    >
+      <div className='hidden sm:block gradient-02 z-0 overflow-hidden right-0 -bottom-[500px]' />
+      <Text colorVariant='secondary' className='w-1/2 pl-32 font-light text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, obcaecati voluptat</Text>
+
+      <div className='relative pt-20 flex w-1/2'>
         <Image
           src='/images/character-1.png'
           alt='character'
