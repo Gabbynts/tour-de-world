391e4242c1402c1e62869a549c66f536995181c8
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-18 21:05:26 +0700

    docs: website preview

--- src/sections/Service.tsx
+++ src/sections/Service.tsx
@@ -9,20 +9,19 @@ function Service() {
   const [active, setActive] = useState('1');
 
   return (
-    <section id='service' className='pt-7 min-h-screen mx-auto flex flex-col gap-y-5 justify-center'>
+    <section id='service' className='pt-7 min-h-screen mx-auto flex flex-col justify-center'>
         <motion.div
         variants={staggerContainer}
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-
       >
         <div className='flex justify-center w-full md:justify-start pr-5 -mt-5 py-3 pl-3'>
             <Text sizeVariant='c' colorVariant='secondary' className='z-10 hover:drop-shadow-2xl'>
             Choose Your World
             </Text>
         </div>
-        <div className='flex flex-col lg:flex-row gap-x-5 min-h-[70vh] px-5 md:pb-10 pb-3'>
+        <div className='flex flex-col lg:flex-row gap-x-5 min-h-[70vh] px-5 gapmd:pb-10 pb-3'>
         {photoWorlds.map((photo, index) => (
           <PhotoCard
             key={photo.id}
