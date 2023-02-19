95d7dc3f08afee83039eba9c262c7d639ac2da8b
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-19 17:29:46 +0700

    fix: data type

--- src/utils/motion.ts
+++ src/utils/motion.ts
@@ -1,48 +1,51 @@
 export const fadeIn = (
-    direction: string, 
-    type: any, 
-    delay: any, 
-    duration: any) => 
-({
-    hidden: {
+  direction: 'left' | 'right' | 'up' | 'down',
+  type: string,
+  delay: number,
+  duration: number
+) => ({
+  hidden: {
     x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
     y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
     opacity: 0,
+  },
+  show: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      type,
+      delay,
+      duration,
+      ease: 'easeOut',
     },
-    show: {
-        x: 0,
-        y: 0,
-        opacity: 1,
-        transition: {
-          type,
-          delay,
-          duration,
-          ease: 'easeOut',
-        },
-    },
-    });
+  },
+});
 
-export const staggerContainer:any = (staggerChildren: any, delayChildren: any) => ({
-    hidden: {},
-    show: {
-      transition: {
-        staggerChildren,
-        delayChildren,
-      },
+export const staggerContainer: any = (
+  staggerChildren: number,
+  delayChildren: number
+) => ({
+  hidden: {},
+  show: {
+    transition: {
+      staggerChildren,
+      delayChildren,
     },
-    });
+  },
+});
 
 export const earth = (direction: string) => ({
-    hidden: {
-      x: direction === 'left' ? '-100%' : '200%',
-      rotate: 180,
+  hidden: {
+    x: direction === 'left' ? '-100%' : '200%',
+    rotate: 180,
+  },
+  show: {
+    x: 0,
+    rotate: 0,
+    transition: {
+      type: 'spring',
+      duration: 1.5,
     },
-    show: {
-      x: 0,
-      rotate: 0,
-      transition: {
-        type: 'spring',
-        duration: 1.5
-      },
-    },
-  });
+  },
+});
