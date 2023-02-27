75a30ce47f939fabd144c53dcb2534af8c474d23
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-27 12:01:19 +0700

    feat: add list photos

---
+++ src/constant/listPhotos.ts
@@ -0,0 +1,47 @@
+export const listPhotos = [
+    {
+        id: '1',
+        imgUrl: '/images/paris.png',
+        title: 'Paris'
+    },
+    {
+        id: '2',
+        imgUrl: '/images/desert.png',
+        title: 'Desert'
+    },
+    {
+        id: '3',
+        imgUrl: '/images/waterfall.png',
+        title: 'Waterfall'
+    },
+    {
+        id: '4',
+        imgUrl: '/images/newyork.png',
+        title: 'NewYork'
+    },
+    {
+        id: '5',
+        imgUrl: '/images/everest.png',
+        title: 'Everest'
+    },
+    {
+        id: '6',
+        imgUrl: '/images/paris.png',
+        title: 'Paris'
+    },
+    {
+        id: '7',
+        imgUrl: '/images/desert.png',
+        title: 'Desert'
+    },
+    {
+        id: '8',
+        imgUrl: '/images/waterfall.png',
+        title: 'Waterfall'
+    },
+    {
+        id: '9',
+        imgUrl: '/images/newyork.png',
+        title: 'NewYork'
+    },
+]
