d56e82151b0cfde00ca0dfdc6ccd2c2442383d80
Gabbynts <gabriella.natasya2003@gmail.com>
2023-02-27 11:57:40 +0700

    feat: add form components

---
+++ src/components/forms/TextArea.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {  RegisterOptions, useFormContext } from 'react-hook-form';
+import get from 'lodash.get';
+
+interface Props {
+  label: string;
+  id: string;
+  className?: string;
+  validation?: RegisterOptions;
+}
+
+function TextArea({ label, id, validation }: Props) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+  const error = get(errors, id);
+
+  return (
+    <div>
+      <div>{label}</div>
+      <div className='pt-2'>
+        <textarea
+          {...register(id, validation)}
+          name={id}
+          id={id}
+          rows={8}
+          className='w-full pl-4 rounded-[22px] bg-[#260D45] text-sm border border-[#d6b6ff] p-4'
+        />
+                {error && (
+        <p className='text-error'>
+          {error?.message?.toString()}
+        </p>
+      )}
+      </div>
+    </div>
+  );
+}
+
+export default TextArea;
