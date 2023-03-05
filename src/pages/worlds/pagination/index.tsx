a3cae7fba8835fd89d5d21f9c82fada5d951d4ee
Gabbynts <gabriella.natasya2003@gmail.com>
2023-03-06 01:36:57 +0700

    feat: add pagination page

---
+++ src/pages/worlds/pagination/index.tsx
@@ -0,0 +1,84 @@
+import Head from 'next/head';
+import Layout from '@/components/layout/Layout';
+import Image from 'next/image';
+import { useInfiniteQuery } from 'react-query';
+import { getPhotos } from '@/pages/api/unsplash';
+import React from 'react';
+import {BsArrowDownCircle} from 'react-icons/bs'
+
+export default function Home() {
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery(
+      'nature',
+      ({ pageParam = 1 }) =>
+        getPhotos(
+          'nature',
+          6,
+          pageParam,
+          't5WxxhORJ7sAw_rEMQVskzTEdzMq4sLKhHWhk99FqSQ'
+        ),
+      {
+        getNextPageParam: (lastPage, allPages) =>
+          lastPage.length === 0 ? undefined : allPages.length + 1,
+      }
+    );
+
+
+  if (status == 'loading') {
+    return (
+      <div className='page_loader'>
+        <div className='loader'></div>
+      </div>
+    );
+  }
+
+  if (status == 'error') {
+    return <div>Error</div>;
+  }
+
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Photos | Tour De World</title>
+          <meta name='TourDeWorld' content='' />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <main className='min-h-screen layout py-24'>
+          <div className='flex flex-col items-center gap-3'>
+            <h1 className='text-[80px] font-thin'>OVERVIEW</h1>
+            <div className='flex flex-wrap gap-5 justify-center'>
+              {data?.pages.map((page, index) => (
+                <React.Fragment key={index}>
+                  {page.map((photo) => (
+                    <div key={photo.id}>
+                      <Image
+                        key={photo.id}
+                        src={photo.urls.regular}
+                        alt={photo.alt_description}
+                        width={300}
+                        height={100}
+                        className='img w-[380px] h-[450px] border-2'
+                      />
+                    </div>
+                  ))}
+                </React.Fragment>
+              ))}
+            </div>
+            <div className='flex justify-center mt-4'>
+              <button
+                onClick={() => fetchNextPage()}
+                disabled={!hasNextPage || isFetchingNextPage}
+                className='text-white transition duration-200 animate-pulse hover:animate-none'
+              >
+                <BsArrowDownCircle className='animate-bounce text-[50px]'/>
+              </button>
+            </div>
+          </div>
+        </main>
+      </Layout>
+    </>
+  );
+}
